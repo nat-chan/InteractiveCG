0561b37faac0a64ee8267fc81d02c3428af7f011
nat-chan <qzrp01@gmail.com>
2018-10-30 19:38:06 +0900

    Makefileわかってないことがわかった。addBrush修理、triangle実装,

---
+++ Impressionist/circleBrush.h
@@ -0,0 +1,23 @@
+//
+// circleBrush.h
+//
+// The header file for Point Brush. 
+//
+
+#ifndef CIRCLEBRUSH_H
+#define CIRCLEBRUSH_H
+
+#include "impBrush.h"
+
+class CircleBrush : public ImpBrush
+{
+public:
+	CircleBrush( ImpressionistDoc* pDoc = NULL, char* name = NULL );
+
+	void BrushBegin( const Point source, const Point target );
+	void BrushMove( const Point source, const Point target );
+	void BrushEnd( const Point source, const Point target );
+	char* BrushName( void );
+};
+
+#endif
