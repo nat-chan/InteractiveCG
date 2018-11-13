8eaa74ff6816460dbe7b93e850fc55e3d105e4ed
nat-chan <qzrp01@gmail.com>
2018-11-13 21:25:34 +0900

    Alt-KでlineBrush追加、動く

---
+++ Impressionist/lineBrush.cpp
@@ -0,0 +1,60 @@
+//
+// lineBrush.cpp
+//
+// The implementation of Point Brush. It is a kind of ImpBrush. All your brush implementations
+// will look like the file with the different GL primitive calls.
+//
+
+#include "impressionistDoc.h"
+#include "impressionistUI.h"
+#include "lineBrush.h"
+
+extern float frand();
+
+LineBrush::LineBrush( ImpressionistDoc* pDoc, char* name ) :
+	ImpBrush(pDoc,name)
+{
+}
+
+void LineBrush::BrushBegin( const Point source, const Point target )
+{
+	ImpressionistDoc* pDoc = GetDocument();
+	ImpressionistUI* dlg=pDoc->m_pUI;
+
+	int size = pDoc->getSize();
+	
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	glPointSize( (float)size );
+
+	BrushMove( source, target );
+}
+
+void LineBrush::BrushMove( const Point source, const Point target )
+{
+	ImpressionistDoc* pDoc = GetDocument();
+	ImpressionistUI* dlg=pDoc->m_pUI;
+
+	//スライダーつけたあと
+
+	if ( pDoc == NULL ) {
+		printf( "LineBrush::BrushMove  document is NULL\n" );
+		return;
+	}
+
+	//SetColorAlpha( source, alpha );
+	SetColor( source );
+	glBegin( GL_POINTS );
+	glVertex2d( target.x, target.y );
+	glEnd();
+}
+
+void LineBrush::BrushEnd( const Point source, const Point target )
+{
+	// do nothing so far
+	glDisable(GL_BLEND);
+
+}
+
