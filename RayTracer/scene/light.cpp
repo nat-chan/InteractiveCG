fe85646b82f90344bb10a3ff6b8a5679cc9b9929
nat-chan <qzrp01@gmail.com>
2018-12-25 19:21:31 +0900

    PointLight::shadowAttenuation、影による減衰を実装

--- RayTracer/scene/light.cpp
+++ RayTracer/scene/light.cpp
@@ -74,7 +74,23 @@ Vec3d PointLight::shadowAttenuation(const Vec3d& P) const
 {
     // YOUR CODE HERE:
     // You should implement shadow-handling code here.
+    Vec3d d = getDirection(P);	// 光源への方向ベクトルをとる
+    d.normalize();
+    ray r( P, d, ray::SHADOW);	// Pからdへ進む、影の判定用のRayを生成
+    isect i;			// 交点情報を保存するためのオブジェクトを生成
+    Vec3d dv = position-P;	// 光源の位置ベクトルと交点の位置ベクトルの差をとる
+    double dist = dv.length();
 
-    return Vec3d(1,1,1);
-
+    /* Rayの交差判定によって返り値を決めよう */
+    if (getScene()->intersect( r, i )) { // Rayがシーン上の何らかのオブジェクトと衝突するか判定
+        //衝突オブジェクトとの距離が, distよりも長い->光源の後ろで衝突するので遮られない
+        Vec3d dv1 =  r.at(i.t) - P; 
+        if(dv1.length() > dist)
+             return Vec3d( 1.0, 1.0, 1.0 );
+        else
+            return Vec3d( 0.0, 0.0, 0.0 );// 衝突するなら0を返す
+    } else
+        return Vec3d( 1.0, 1.0, 1.0 );// 衝突しなければ1を返す
 }
+
+
